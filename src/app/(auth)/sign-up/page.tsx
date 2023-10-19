480392032ff2fc17a587cc6b05e7019a61c8ec65
frolleks <frolleks@hotmail.com>
2023-10-19 20:08:44 +0700

    feat: redirect to root when a logged in user tries to go to the sign up page

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,11 +1,23 @@
 import { SignUpForm } from "@/components/sign-up-form";
+import { auth } from "@/lib/auth/lucia";
 
-export default function SignUp() {
-  return (
-    <div className="w-full h-screen flex justify-center items-center">
-      <div className="w-96">
-        <SignUpForm />
+import * as context from "next/headers";
+import { redirect } from "next/navigation";
+
+export default async function SignUp() {
+  const authRequest = auth.handleRequest("GET", context);
+  const cookieSession = await authRequest.validate();
+  const bearerSession = await authRequest.validateBearerToken();
+
+  if (!cookieSession || bearerSession) {
+    return (
+      <div className="w-full h-screen flex justify-center items-center">
+        <div className="w-96">
+          <SignUpForm />
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    redirect("/");
+  }
 }
