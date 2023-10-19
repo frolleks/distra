a6eb53b10a9cf447f6cc1caa0615c1a2152fcd24
frolleks <frolleks@hotmail.com>
2023-10-19 17:38:25 +0700

    refactor: use next.js middleware on lucia

--- src/lib/auth/lucia.ts
+++ src/lib/auth/lucia.ts
@@ -1,5 +1,6 @@
 import { lucia } from "lucia";
 import { postgres as postgresAdapter } from "@lucia-auth/adapter-postgresql";
+import { nextjs_future } from "lucia/middleware";
 
 import { queryClient } from "@/lib/db";
 
@@ -10,4 +11,9 @@ export const auth = lucia({
     session: "user_session",
   }),
   env: process.env.NODE_ENV === "development" || "test" ? "DEV" : "PROD",
+  middleware: nextjs_future(),
+
+  sessionCookie: {
+    expires: false,
+  },
 });
