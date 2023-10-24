10dae23004330d059355b5a9bf3a0400b39061f0
frolleks <frolleks@hotmail.com>
2023-10-24 16:37:45 +0700

    feat: add a dropdown menu to account nav

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,4 +1,4 @@
-import { Menu, Plus } from "lucide-react";
+import { Menu, PersonStanding, Plus, User } from "lucide-react";
 
 import { Comfortaa } from "next/font/google";
 import * as context from "next/headers";
@@ -6,6 +6,15 @@ import Image from "next/image";
 import Link from "next/link";
 import { Button } from "./ui/button";
 import { getSession, getUser } from "@/lib/auth/utils";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 
 const display = Comfortaa({ subsets: ["latin"] });
 
@@ -15,7 +24,40 @@ async function AuthNav() {
   if (session) {
     const user = await getUser(context);
 
-    return <p className="text-sm">{user?.username}</p>;
+    return (
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <Avatar>
+            <AvatarImage
+              src={user?.profilePicture!}
+              alt="Your profile picture"
+            />
+            <AvatarFallback>{user?.username.charAt(0)}</AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56">
+          <DropdownMenuGroup>
+            <p className="px-2 pt-1 text-sm font-semibold">
+              {user?.displayName}
+            </p>
+            <p className="px-2 pb-1 text-sm text-muted-foreground">
+              @{user?.username}
+            </p>
+          </DropdownMenuGroup>
+          <DropdownMenuSeparator />
+          <DropdownMenuGroup>
+            <DropdownMenuItem>
+              <Link href={`/user/${user?.id}`}>
+                <div className="flex items-center">
+                  <User className="mr-1.5 w-4 h-4" />
+                  <span>Your channel</span>
+                </div>
+              </Link>
+            </DropdownMenuItem>
+          </DropdownMenuGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    );
   }
 
   return (
@@ -28,7 +70,7 @@ async function AuthNav() {
 export async function Navbar() {
   return (
     <nav className="sticky top-0 z-10">
-      <div className="flex justify-between items-center p-2 bg-background border-b">
+      <div className="flex justify-between items-center p-3 bg-background border-b">
         <div className="flex items-center gap-1.5">
           <Button className="max-sm:hidden" variant="ghost" size="icon">
             <Menu size={22} />
